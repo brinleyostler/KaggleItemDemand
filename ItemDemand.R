3e5c9c7db55e8dc8801fcc97d3ea6b55b0ddd7bb
Brinley Ostler <brinley.ostler@gmail.com>
2024-11-25 09:49:28 -0700

    add h2o model

--- ItemDemand.R
+++ ItemDemand.R
@@ -13,14 +13,18 @@ train = vroom("train.csv")
 
 ## Filter to 1 store & item
 storeItemTrain1 <- train %>%
-  filter(store==8, item==25)
+  filter(store==8, item==25) %>% 
+  mutate(item=as.factor(item))
 storeItemTest1 <- test %>% 
-  filter(store==8, item==25)
+  filter(store==8, item==25) %>% 
+  mutate(item=as.factor(item))
 
 storeItemTrain2 <- train %>%
-  filter(store==5, item==35)
+  filter(store==5, item==35) %>% 
+  mutate(item=as.factor(item))
 storeItemTest2 <- test %>% 
-  filter(store==5, item==35)
+  filter(store==5, item==35) %>% 
+  mutate(item=as.factor(item))
 
 ## CV SPLIT
 cv_split1 <- time_series_split(storeItemTrain1, assess="3 months", cumulative = TRUE)
@@ -72,20 +76,16 @@ sales_recipe1 = recipe(sales~., storeItemTrain1) %>%
   step_date(date, features=c("dow", "month", "doy", "decimal")) %>%                 
   step_mutate(date_decimal=as.numeric(date_decimal)) %>% 
   step_mutate_at(date_dow, fn=factor) %>% 
-  step_mutate_at(date_month, fn=factor) %>% 
-  #step_range(date_doy, min=0, max=pi) %>%
-  #step_mutate(sinDOY=sin(date_doy), cosDOY=cos(date_doy)) %>% 
-  #step_lag(sales, lag=7) %>% 
+  step_mutate_at(date_month, fn=factor)  %>%
+  step_timeseries_signature(date) %>% 
   step_rm(c(store, item, date_doy))
 
 sales_recipe2 = recipe(sales~., storeItemTrain2) %>% 
   step_date(date, features=c("dow", "month", "doy", "decimal")) %>%                 
   step_mutate(date_decimal=as.numeric(date_decimal)) %>% 
   step_mutate_at(date_dow, fn=factor) %>% 
-  step_mutate_at(date_month, fn=factor) %>% 
-  #step_range(date_doy, min=0, max=pi) %>%
-  #step_mutate(sinDOY=sin(date_doy), cosDOY=cos(date_doy)) %>% 
-  #step_lag(sales, lag=7) %>% 
+  step_mutate_at(date_month, fn=factor) %>%
+  step_timeseries_signature(date) %>% 
   step_rm(c(store, item, date_doy))
 
 prepped1 = prep(sales_recipe1)
@@ -93,13 +93,26 @@ prepped2 = prep(sales_recipe2)
 baked1 = bake(prepped1, new_data=storeItemTrain1)
 baked2 = bake(prepped2, new_data=storeItemTrain2)
 
-#### PROPHET MODEL ####
-prophet_model1 <- prophet_reg() %>%
-  set_engine(engine = "prophet") %>%
-  fit(sales ~ date, data = training(cv_split1))
-prophet_model2 <- prophet_reg() %>%
-  set_engine(engine = "prophet") %>%
-  fit(sales ~ date, data = training(cv_split2))
+#### H2O MODEL ####
+h2o.init(
+  nthreads = -1,
+  ip       = 'localhost',
+  port     = 54321)
+
+h2o_spec <- automl_reg(mode = 'regression') %>%
+  set_engine(engine = 'h2o',
+    max_runtime_secs = 5, 
+    max_runtime_secs_per_model = 3,
+    max_models = 3,
+    nfolds = 5,
+    exclude_algos = c("DeepLearning"),
+    verbosity = NULL,
+    seed = 786)
+
+h2o_model1 <- h2o_spec %>%
+  fit(sales~., data = storeItemTrain1)
+h2o_model2 <- h2o_spec %>%
+  fit(sales~., data = storeItemTrain2)
 
 
 #### CV ####
@@ -114,9 +127,9 @@ cv_split2 %>%
 
 #### WORKFLOW ####
 # Run the CV
-cv_results1 <- modeltime_calibrate(prophet_model1,
+cv_results1 <- modeltime_calibrate(h2o_model1,
                                   new_data = testing(cv_split1))
-cv_results2 <- modeltime_calibrate(prophet_model2,
+cv_results2 <- modeltime_calibrate(h2o_model2,
                                    new_data = testing(cv_split2))
 
 ## Visualize results
