5e6b2fbba78d4d1c17cbdee01b90c067c439d711
Brinley Ostler <brinley.ostler@gmail.com>
2024-12-04 18:18:50 -0700

    add LGBM model

--- ItemDemand.R
+++ ItemDemand.R
@@ -11,40 +11,49 @@ library(plotly)
 test = vroom("/kaggle/input/demand-forecasting-kernels-only/test.csv")
 train = vroom("/kaggle/input/demand-forecasting-kernels-only/train.csv")
 
-### PROPHET MODEL ####
+### LIGHTGBM MODEL ####
 nStores <- max(train$store)
 nItems <- max(train$item)
 
+## Recipe
+store_recipe <- recipe(sales~., data=train) %>%
+  step_date(date, features = c("dow", "month", "decimal", "doy", "year")) %>% 
+  step_holiday(date, role = "predictor") %>% 
+  step_range(date_doy, min = 0, max = pi) %>%                                 
+  step_mutate(sinDOY = sin(date_doy), cosDOY = cos(date_doy)) %>%             
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(sales)) %>%     
+  step_normalize(all_numeric_predictors()) %>%                                
+  step_rm(date, item, store)                                                  
+
+## Model
+lgbm_model <- boost_tree(tree_depth=1, # tune()
+                         learn_rate=0.1,  # tune()
+                         trees=1000) %>%
+  set_engine("lightgbm") %>%
+  set_mode("regression")
+
+## Workflow
+lgbm_wf <- workflow() %>%
+  add_recipe(store_recipe) %>%
+  add_model(lgbm_model)
+
+### FOR LOOP ####
 for(s in 1:nStores){
   for(i in 1:nItems){
     storeItemTrain <- train %>%
-    filter(store==s, item==i)
+      filter(store==s, item==i)
     storeItemTest <- test %>%
-    filter(store==s, item==i)
+      filter(store==s, item==i)
     
     ## Fit storeItem models here
-    cv_split <- time_series_split(storeItemTrain, assess="3 months", cumulative = T)
-    
-    prophet_model <- prophet_reg() %>%
-      set_engine(engine = "prophet") %>%
-      fit(sales ~ date, data = training(cv_split))
-    
-    cv_results <- modeltime_calibrate(prophet_model,
-                                      new_data = testing(cv_split))
-    
-    fullfit <- cv_results %>%
-      modeltime_refit(data=storeItemTrain)
+    fitted_wf <- lgbm_wf %>%
+      fit(data=storeItemTrain)
     
     ## Predict storeItem sales
-    preds <- fullfit %>%
-      modeltime_forecast(
-        new_data = storeItemTest,
-        actual_data = storeItemTrain
-      ) %>%
-      filter(!is.na(.model_id)) %>%
-      mutate(id=storeItemTest$id) %>%
-      select(id, .value) %>%
-      rename(sales=.value)
+    preds <- predict(fitted_wf, new_data=storeItemTest) %>%
+      bind_cols(storeItemTest) %>%
+      rename(sales=.pred) %>%
+      select(id, sales)
     
     ## Save storeItem predictions
     if(s==1 & i==1){
